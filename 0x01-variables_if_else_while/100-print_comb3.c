85584bea84d0004cd4e6c6e68df64a42bff5d232
william-4 <mwangiiwilliam@gmail.com>
2022-03-13 22:48:19 +0300

    task 10 adding space befor opening bracket on line 19

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,7 +16,7 @@ int main(void)
 
 	for (a = '0'; a <= '9'; a++)
 	{
-		for(b = '0'; b <= '9'; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
 			if (a != b && b > a)
 			{
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,7 +20,7 @@ int main(void)
 			{
 				for (d = '0'; d <= '9'; d++)
 				{
-					if (b == c && a == d )
+					if (b == c && a == d)
 					{
 						putchar(a);
 						putchar(b);
