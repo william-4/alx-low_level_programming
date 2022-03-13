d8cc2b6962a7a214a1e7ddb76a772dca9bf57010
william-4 <mwangiiwilliam@gmail.com>
2022-03-13 19:33:55 +0300

    more corrections

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,25 @@
 */
 int main(void)
 {
-	int num = '0';
+	int num;
 
-	while (num >= '0' && num < '9')
-	{
-		putchar(num);
-		putchar(',');
-		putchar(' ');
-		num++;
-	}
-	if (num == '9')
-		putchar(num);
+	/*
+	 *loop for numbers 0-9
+	 *if number is 0-9, print number
+	 *then if number is 0-8, add comma plus space
+	 *else if number is 9, add only space
+	 */
+	for (num == '0'; num <= '9'; num++)
+		if (num >= '0')
+		{
+			putchar(num);
+			if (num >= '0' && num <= '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+				putchar(' ');
+		}
 	return (0);
 }
