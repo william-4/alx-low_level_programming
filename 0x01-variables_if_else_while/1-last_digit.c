fbe7d0dc9b284b8e4d8a1636d23dc695491cf77e
william-4 <mwangiiwilliam@gmail.com>
2022-03-13 14:04:26 +0300

    task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - prints the last digit stored in variable n
+ *@n: the input to be used
+ *@p: stores the remainder after modulo operator
+ *Return: 0(Success)
+ */
+int main(void)
+{
+	int n;
+	int p;
+
+	scanf("%d", &n);
+      	p = n % 10;
+	if (p>5)
+	{
+		printf("Last digit of %d is greater than 5\n", n);
+	}
+	else if (p==0)
+	{
+		printf("Last digit of %d is 0\n", n);
+	}
+	else if (p<6 && p!=0)
+	{
+		printf("Last string of %d is less than 6 and not 0\n", n);
+	}
+	return (0);
+}
