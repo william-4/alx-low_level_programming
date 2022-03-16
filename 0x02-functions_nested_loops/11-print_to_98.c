ae91b9346288dade58b5db1ab0789339f65466e4
william-4 <mwangiiwilliam@gmail.com>
2022-03-16 14:44:50 +0300

    task 11 corrections

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,18 +10,39 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n > 98)
 	{
-		if (i == 98)
+		for (i = n; i >= 98; i--)
 		{
-			_putchar(i);
-			_putchar('\n');
-		}
-		else
-		{
-			_putchar(i);
-			_putchar(',');
-			_putchar(' ');
+			if (i == 98)
+			{
+				_putchar(i);
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(i);
+				_putchar(',');
+				_putchar('\n');
+			}
 		}
 	}
+	else if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i == 98)
+			{
+				_putchar(i);
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(i);
+				_putchar(',');
+				_putchar('\n');
+			}
+		}
+	}
+
 }
