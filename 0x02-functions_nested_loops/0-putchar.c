d0392f8197e9aa6dd63f4858d3988e3771d47e9e
william-4 <mwangiiwilliam@gmail.com>
2022-03-16 10:07:33 +0300

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include <unistd.h>
 
 /**
- *_putchar - Prints putchar
+ *main - Prints putchar\n
  *
  *Return: 0(Success)
  */
 int main(void)
 {
 	const char msg[] = "_putchar\n";
+
 	write(STDOUT_FILENO, msg, sizeof(msg) - 1);
 
 	return (0);
