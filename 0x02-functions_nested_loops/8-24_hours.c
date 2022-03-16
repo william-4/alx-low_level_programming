74395ff16b115b247643d958636e3caca8e06f6f
william-4 <mwangiiwilliam@gmail.com>
2022-03-16 22:27:39 +0300

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *jack_bauer - count 24 hours
+ *
+ *Return: void
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar('0' + (hour / 10));
+			_putchar('0' + (hour % 10));
+			_putchar(':');
+			_putchar('0' + (minute / 10));
+			_putchar('0' + (minute % 10));
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,4 @@ int _abs(int);
 int print_last_digit(int);
 int add(int, int);
 void print_to_98(int n);
+void jack_bauer(void);
