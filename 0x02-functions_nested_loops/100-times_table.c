0bcc930b9b641c3efd9a3914e2dcfc20665962e5
william-4 <mwangiiwilliam@gmail.com>
2022-03-17 00:21:59 +0300

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,42 +10,48 @@ void print_times_table(int n)
 {
 	int a, b, prod;
 
-	for (a = 0; a <= n; a++)
+	if (n < 0 || n > 15)
 	{
-		for (b = 0; b <= n; b++)
+		for (a = 0; a <= n; a++)
 		{
-			prod = a * b;
-			if (prod < 10 && b != n && b != 0)
+			for (b = 0; b <= n; b++)
 			{
-				_putchar(' ');
-				_putchar(prod + '0');
-				_putchar(',');
-				_putchar(' ');
+				prod = a * b;
+				if (prod < 10 && b != n && b != 0)
+				{
+					_putchar(' ');
+					_putchar(prod + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+				else if (prod >= 10 && b != n)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar((prod / 10) + '0');
+					_putchar((prod % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+				else if (prod < 10 && b == n && b != 0)
+				{
+					_putchar(' ');
+					_putchar(prod + '0');
+				}
+				else if (b == 0)
+				{
+					_putchar('0' + prod);
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((prod / 100) + '0');
+					_putchar((prod / 10) + '0');
+					_putchar((prod % 10) + '0');
+				}
 			}
-			else if (prod >= 10 && b != n)
-			{
-				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else if (prod < 10 && b == n && b != 0)
-			{
-				_putchar(' ');
-				_putchar(prod + '0');
-			}
-			else if (b == 0)
-			{
-				_putchar('0' + prod);
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
+	
 }
