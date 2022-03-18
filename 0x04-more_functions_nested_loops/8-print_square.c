bd70766d7529f1c1009d8b99289daa8afe345485
william-4 <mwangiiwilliam@gmail.com>
2022-03-18 03:12:25 +0300

    task 8 corrections

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,11 +15,12 @@ void print_square(int size)
 	{
 		for (i = 1; i <= size; i++)
 		{
+			_putchar('#');
 			for (j = 1; j < size; j++)
 			{
 				_putchar('#');
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
