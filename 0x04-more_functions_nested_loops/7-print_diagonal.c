bf3a7ff1fa77b60a2a7f7b28365102debcf64e27
william-4 <mwangiiwilliam@gmail.com>
2022-03-18 03:07:19 +0300

    task 7 corrections

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,9 +13,9 @@ void print_diagonal(int n)
 		_putchar('\n');
 	else
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			for (j = 0; j <= i; j++)
+			for (j = 1; j <= i; j++)
 			{
 				_putchar(' ');
 			}
