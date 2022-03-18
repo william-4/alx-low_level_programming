2b689f57634650b4311fe622f381620e898fb17d
william-4 <mwangiiwilliam@gmail.com>
2022-03-18 03:22:57 +0300

    task 8 corrections

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,24 +13,24 @@ void main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("%s", "FizzBuzz");
+			printf("%s ", "FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("%s", "Fizz");
+			printf("%s ", "Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("%s", "Buzz");
+			printf("%s ", "Buzz");
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 		}
-		if (i != 100)
+		/*if (i != 100)
 		{
 			printf(" ");
-		}
+			}*/
 	}
 	printf("\n");
 }
