b5ece9542ec0eb88c86b35523a548678861623c2
william-4 <mwangiiwilliam@gmail.com>
2022-03-28 13:26:12 +0300

    betty linter corrections

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,11 +8,11 @@
  *
  *Return: pointer to the memory area s
  */
-char* _memset(char* s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		*s = b;
 		s += 1;
