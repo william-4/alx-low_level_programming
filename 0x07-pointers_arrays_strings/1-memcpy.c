a0209efd8f101209146bdf82911b8d05a3806a6f
william-4 <mwangiiwilliam@gmail.com>
2022-03-28 14:00:07 +0300

    task 1 correction

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,13 +10,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
-	int j = 0;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		dest[j] = src[i];
-		j++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
