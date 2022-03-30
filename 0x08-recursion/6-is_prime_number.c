4bb707968f7aa81cb95e911a3a243ab497f7f8dd
william-4 <mwangiiwilliam@gmail.com>
2022-03-31 01:58:57 +0300

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ *helperFun - checks if a number is prime
+ *@a: input number
+ *@b: int to use to check
+ *
+ *Return: int
+ */
+int helperFun(int a, int b)
+{
+	if (b < a)
+	{
+		if (a % b == 0)
+			return (0);
+		else
+			return (helperFun(a, b + 1));
+	}
+	else
+		return (1);
+}
+
+/**
  *is_prime_number - checks if a number is a prime number
  *@n: input int to be checked
  *
@@ -8,5 +28,12 @@
  */
 int is_prime_number(int n)
 {
-	
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (helperFun(n, 2));
+	}
 }
