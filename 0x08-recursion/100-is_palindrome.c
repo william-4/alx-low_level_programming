451f6a629b10b59b4ea4651e401d183f21a2c9f4
william-4 <mwangiiwilliam@gmail.com>
2022-03-31 02:09:57 +0300

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,42 @@
 #include "main.h"
 
 /**
+ *str_len - returns the lenght of a string.
+ *@str: input string
+ *
+ *Return: int (length of the input string)
+ */
+int str_len(char *str)
+{
+	if (*str)
+	{
+		return (1 + str_len(str + 1));
+	}
+	return (0);
+}
+
+/**
+  *helperFun - checks if two strings are palindrome
+  *@s: pointer to string.
+  *@first: first index.
+  *@last: last index.
+  *
+  *Return: 1 if palindrome and 0 if not
+  */
+int helperFun(char *s, int first, int last)
+{
+	if (first < last && s[first] == s[last])
+	{
+		return (helperFun(s, first + 1, last - 1));
+	}
+	if (s[first] != s[last])
+	{
+		return (0);
+	}
+	return (1);
+}
+
+/**
  *is_palindrome - checks if a string is a palindrome
  *@s: input string
  *
@@ -8,5 +44,13 @@
  */
 int is_palindrome(char *s)
 {
-	
+	int a;
+	int b;
+
+	a = 0;
+	b = str_len(s) - 1;
+
+	if (!*s)
+		return (1);
+	return (helperFun(s, a, b));
 }
