b84881361ea328b567e7c1554acd5c2f5886f532
william-4 <mwangiiwilliam@gmail.com>
2022-04-03 23:37:17 +0300

    corrections

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc -c -Wall -Werror -Wextra -pedanitc -std=gnu89 *c
-ar rc liball.a ./*o
+gcc -c -Wall -Werror -Wextra -pedanitc -std=gnu89 *.c
+ar rc liball.a ./*.o
 ranlib liball.a  #used to regenerate the index
