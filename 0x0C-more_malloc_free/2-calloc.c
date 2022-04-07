f9e3902560714df4a5f9e4169146b6586536e1d4
william-4 <mwangiiwilliam@gmail.com>
2022-04-07 22:23:25 +0300

    New Commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
  * _calloc - allocates memory for an array using malloc (same as calloc)
@@ -22,6 +24,52 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	/* initialize the values at each index to zero */
 	for (i = 0; i < nmemb; i++)
-		*(int *)ptr = 0;
+		*(int *)(ptr + i) = 0;
 	return (ptr);
+}
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *a;
+
+    a = _calloc(98, sizeof(char));
+    strcpy(a, "Best");
+    strcpy(a + 4, " School! :)\n");
+    a[97] = '!';
+    simple_print_buffer(a, 98);
+    free(a);
+    return (0);
 }
