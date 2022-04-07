8981abf41e966b2a9199cd9263cbe832c7db7c9f
william-4 <mwangiiwilliam@gmail.com>
2022-04-07 23:56:47 +0300

    new commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,16 +11,18 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
-	int i;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	/* allocate memory for the elements */
 	ptr = malloc(nmemb * size);
-	/* if malloc fails(returns NULL), return NULL */
+
+/* if malloc fails(returns NULL), return NULL */
 	if (ptr == NULL)
 		return (NULL);
-	/* initialize the values at each index to zero */
+
+/* initialize the values at each index to zero */
 	for (i = 0; i < (nmemb * size); i++)
 		*((char *)(ptr) + i) = 0;
 	return (ptr);
