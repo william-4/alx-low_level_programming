4c268f6ec6fb23fa3a44620af6a92fb1feab28fb
william-4 <mwangiiwilliam@gmail.com>
2022-04-08 14:11:19 +0300

    new commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,6 @@
  */
 int main(void)
 {
-	write(2, "and that piee of art is useful\" - Dora
-Korpar, 2015-10-19", 59);
-	return (1);
+write(2, "and that piee of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
