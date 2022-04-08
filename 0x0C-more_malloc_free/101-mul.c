40d094e770176dd8d2dda2287bef890d4f8a2fa3
william-4 <mwangiiwilliam@gmail.com>
2022-04-08 13:44:57 +0300

    new commit

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mul - function that multiplies two positive integers
+ * @num1: input integer
+ * @num2: input integer
+ * 
+ * Return: 0(Success)
+ */
+int mul(int num1, int num2)
+{
+        int *ptr;
+        int ans;
+
+        if (num1 && num2)
+        {
+                ptr = malloc(sizeof(int));
+                ans = num1 * num2;
+                *ptr = ans;
+                printf("%d\n", *ptr);
+                free(ptr);
+        }
+        else
+        {
+                printf("Error\n");
+                exit(98);
+        }
+                return (0);
+}
+
+/**
+ * main - calls mul function with 2 integers arguments
+ * 
+ * @argc: count of input string arguments
+ * @argv: string arguments with the first being the name of the program 
+ * 
+ * Return: 0(Success) 
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+        int a = atoi(argv[1]);
+        int b = atoi(argv[2]);
+
+        /* atoi converts input string to int */
+        mul(a, b);
+        return (0);
+}
