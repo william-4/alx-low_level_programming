b344bd230e18ff770fb1b978a9b9124026692563
william-4 <mwangiiwilliam@gmail.com>
2022-04-11 22:10:16 +0300

    new commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,6 +7,8 @@
  * @name: name of the dog
  * @age: the age of the dog
  * @owner: the owner of the dog
+ *
+ * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
