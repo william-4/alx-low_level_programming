fd6d59af540114f161dead050ee7d63ba83e2611
william-4 <mwangiiwilliam@gmail.com>
2022-04-11 22:19:36 +0300

    new commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_dog - prints a 'struct dog'
@@ -9,7 +10,21 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n", (*d).owner);/* dereference then acces member */
+	if (d == NULL)
+		return;
+	else
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		if (d->age == NULL)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %.6f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", (*d).owner);
+	}
 }
