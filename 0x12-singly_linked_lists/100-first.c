e52412245853cc0bccd02e7a2c2c5bb3bd649d28
william-4 <mwangiiwilliam@gmail.com>
2022-04-21 22:30:17 +0300

    correct output

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
  */
 void print_first(void)
 {
-	printf("You're a beat! and yet, you must allow,\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
