72d14f959a598fab1774c30f08c9c55d0e640326
william-4 <mwangiiwilliam@gmail.com>
2022-04-27 02:58:14 +0300

    new commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,24 +1,6 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked listint_t list
- * @h: pointer to the head of the listint_t list
- *
- * Return: size_t (number of elements in the linked list)
- */
-size_t listint_len(const listint_t *h)
-{
-	int i = 0;
-
-	while (h != NULL)
-	{
-		i++;
-		h = (*h).next; /* h->next */
-	}
-	return (i);
-}
-
-/**
  * get_nodeint_at_index - returns nth node of a linked list
  * @head: pointer to the first node in the list
  * @index: index of node to be removed
@@ -32,12 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
-
-	int a = listint_len(head);
-
 	temp = head;
-
-	/*if (index > (a - 1))*/
 
 	while (i < index)
 	{
