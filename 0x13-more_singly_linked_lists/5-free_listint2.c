7d299af6b1836b0b9c04da74a66e60c08d859257
william-4 <mwangiiwilliam@gmail.com>
2022-04-27 00:41:19 +0300

    new commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to a pointer the first node
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
