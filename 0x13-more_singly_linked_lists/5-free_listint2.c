819dd3d6f66a9eb252be11c4836c4c68850eeaf5
william-4 <mwangiiwilliam@gmail.com>
2022-04-27 00:47:53 +0300

    corrections

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head)
+	if (*head == NULL)
+		return;
+	else
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		while (*head)
+		{
+			temp = (*head)->next;
+			free(*head);
+			*head = temp;
+		}
 	}
 	*head = NULL;
 }
