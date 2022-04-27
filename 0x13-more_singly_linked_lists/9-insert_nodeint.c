6ce0bcc0e9e5cef1fc200f986f70921183c6610b
william-4 <mwangiiwilliam@gmail.com>
2022-04-27 04:02:45 +0300

    new commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t  *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		temp = temp->next;
 		if (temp == NULL)
-			return NULL;
+			return (NULL);
 		i++;
 	}
 	new->next = temp->next;
