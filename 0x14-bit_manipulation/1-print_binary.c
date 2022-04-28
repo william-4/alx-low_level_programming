d8a7f581f340ba81e9472220a61ecacaaa53f708
william-4 <mwangiiwilliam@gmail.com>
2022-04-28 23:31:00 +0300

    corrections

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,8 +9,12 @@
  */
 void print_binary(unsigned long int n)
 {
+	if (!n)
+		return;
+	if (n == 0)
+		_putchar('0');
 	if (n > 1)
 		print_binary(n >> 1);
 
-	printf("%ld", n & 1);
+	_putchar((n & 1) + '0');
 }
