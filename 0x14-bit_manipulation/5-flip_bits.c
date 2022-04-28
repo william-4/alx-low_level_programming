1b56fc3b53cb20965ae85f08eb0c53fb8c13c42d
william-4 <mwangiiwilliam@gmail.com>
2022-04-29 01:24:45 +0300

    updated my code

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int get_bit(unsigned long int n, unsigned int index);
+
 /**
  * flip_bits - returns no of bit to flip to get from n to m
  * @n: interger (from)
@@ -9,12 +11,37 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int temp;
+	int i = 0;
+	int count = 0;
+	int index = 0;
 
-	if (!n && !m)
+	while (i < 64)
+	{
+		if (get_bit(n, index) != get_bit(m, index))
+		{
+			count++;
+		}
+		i++;
+		index++;
+	}
+	return (count);
+}
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: input decimal number
+ * @index: index to check
+ *
+ * Return: value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int ans;
+
+	if (index > 64)
 		return (-1);
 
-	
+	ans = (n >> index) & 1;
 
-	return (1);
+	return (ans);
 }
