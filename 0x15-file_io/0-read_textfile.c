6c18cdbabca1e818b94988f6cee89d4a9a2c91a5
william-4 <mwangiiwilliam@gmail.com>
2022-05-03 23:38:32 +0300

    syntax corrections

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buf = malloc(letters);
 
-	fd = open(filename, 0_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
