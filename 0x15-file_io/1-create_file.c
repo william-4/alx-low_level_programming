604a69de9a38dfb10e1facae172213791e640d15
william-4 <mwangiiwilliam@gmail.com>
2022-05-04 00:21:04 +0300

    new commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: string to write to the file
+ *
+ * Return: 1 on success / -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w;
+	int len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONGLY, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	while (text_content[len])
+		len++;
+
+	w = write(fd, text_content, len);
+	if (w == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
